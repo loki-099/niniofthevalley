fe4ef47442d7beb9d9cafed22b81b97b59ab0df5
mischiefluis <loidatolentino358@gmail.com>
2024-07-06 22:01:08 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import Home from "./pages/Home";
+import { useEffect } from "react";
+
 function App() {
+  let music = new Audio('/assets/music.mp3')
+  music.volume = 0.5
+
+  useEffect(() => {
+    const handleSpace = (event) => {
+      if (event.key === ' ') {
+        console.log("Pressed " + event.key);
+        music.play()
+      }
+    }
+    window.addEventListener('keydown', handleSpace)
+    return () => {
+      window.removeEventListener('keydown', handleSpace);
+    };
+  }, [])
+
   return (
-    <>
-      
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Home/>
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+
+const Home = () => {
+  return (
+    <div className="w-full max-w-[1366px] h-full max-h-[768px] bg-[url('/assets/bg.png')] bg-auto bg-left-top relative flex justify-center items-center overflow-hidden">
+      <div className='absolute left-0 top-0 flex cloud-run'>
+        <img src="/assets/clouds.png" alt="clouds" className='w-auto max-w-fit h-[180px] px-10'/>
+        <img src="/assets/clouds.png" alt="clouds" className='w-auto max-w-fit h-[180px] px-10'/>
+      </div>
+      <img src="/assets/ground.png" alt="ground" className='absolute bottom-0'/>
+      <div className='relative z-10 pb-4'>
+        <h1 className='text-[#111111] text-center text-8xl uppercase leading-[70px] title'>Nini<br/>of<span className='tracking-[-20px]'> </span>the<br/>Valley</h1>
+        <p className='mt-14 text-4xl text-[#111111] text-center animate-pulse'>Press space to start</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
